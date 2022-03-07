d188a9eeac252db1d196b2a70a963d74ff8cb6ae
swapnil girish pawar <swapnilgirishpawar@gmail.com>
2022-03-07 17:35:22 +0530

    7/03/2022 Commit

---
+++ Operators.py
@@ -0,0 +1,45 @@
+"""#Arithmetic Operators
+x = 15
+y = 4
+print('x + y =',x+y) # Output: x + y = 19
+print('x - y =',x-y) # Output: x - y = 11
+print('x * y =',x*y) # Output: x * y = 60
+print('x / y =',x/y) # Output: x / y = 3.75
+print('x // y =',x//y) # Output: x // y = 3
+print('x ** y =',x**y) # Output: x ** y = 50625
+
+#Camparison Operator
+x = 10
+y = 12
+print('x > y is',x>y) # Output: x > y is False
+print('x < y is',x<y) # Output: x < y is True
+print('x == y is',x==y) # Output: x == y is False
+print('x != y is',x!=y)# Output: x != y is True
+print('x >= y is',x>=y) # Output: x >= y is False
+print('x <= y is',x<=y) # Output: x <= y is True
+
+#Logical Operator
+x = True
+y = False
+print('x and y is',x and y)
+print('x or y is',x or y)
+print('not x is',not x)
+
+#Identity Operators
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+print(x1 is not y1) # Output: False
+print(x2 is y2) # Output: True
+print(x3 is y3) # Output: False
+"""
+#Membership Operator
+x = 'Hello world'
+y = {1:'a',2:'b'}
+print('H' in x) # Output: True
+print('hello' not in x) # Output: True
+print(1 in y) # Output: True
+print('a' in y) # Output: False
